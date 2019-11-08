ffb9094ec80a55e05b1f4ce1bb9f8bd57ef55d5f
Germaine Pannell <me@germainep.com>
2019-11-07 20:19:27 -0500

    added state to show and input member info

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import TeamList from './components/TeamList'
+import MemberForm from './components/MemberForm'
 
 function App() {
+  const [members, setMembers] = useState([
+    {
+      id: 1,
+      name: 'Jimmy',
+      email: 'jimmy@devjob.com',
+      title: 'Backend Dev'
+    }
+  ])
+
+  const addNewMember = member => {
+    const newMember = setMembers([...members, member])
+  }
   return (
     <div className="App">
-      <TeamList/>
+      <TeamList members={members}/>
+      <MemberForm addNewMember={addNewMember}/>
     </div>
   );
 }
